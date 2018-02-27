323c8081b8c1552402eac18b434ba59ee198fade
Danilo Lima <danilomourag@gmail.com>
2018-02-27 14:12:33 -0300

    Fixes FriendsController

--- WhosGotMyGames/Controllers/FriendsController.cs
+++ WhosGotMyGames/Controllers/FriendsController.cs
@@ -28,129 +28,123 @@ namespace WhosGotMyGames.Controllers
             return View(_unitOfWork.FriendsRepository.GetAll());
         }
 
-        //// GET: Friends/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Friends/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //    var friend = await _context.Friend
-        //        .SingleOrDefaultAsync(m => m.FriendId == id);
-        //    if (friend == null)
-        //    {
-        //        return NotFound();
-        //    }
+            var friend = _unitOfWork.FriendsRepository.Find(f => f.FriendId == id.Value).FirstOrDefault();
+            if (friend == null)
+            {
+                return NotFound();
+            }
 
-        //    return View(friend);
-        //}
+            return View(friend);
+        }
 
-        //// GET: Friends/Create
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
+        // GET: Friends/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-        //// POST: Friends/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("FriendId,Name")] Friend friend)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(friend);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(friend);
-        //}
+        // POST: Friends/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("FriendId,Name")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.FriendsRepository.Add(friend);
+                _unitOfWork.Complete();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(friend);
+        }
 
-        //// GET: Friends/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Friends/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //    var friend = await _context.Friend.SingleOrDefaultAsync(m => m.FriendId == id);
-        //    if (friend == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(friend);
-        //}
+            var friend = _unitOfWork.FriendsRepository.Get(id.Value);
+            if (friend == null)
+            {
+                return NotFound();
+            }
+            return View(friend);
+        }
 
-        //// POST: Friends/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("FriendId,Name")] Friend friend)
-        //{
-        //    if (id != friend.FriendId)
-        //    {
-        //        return NotFound();
-        //    }
+        // POST: Friends/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("FriendId,Name")] Friend friend)
+        {
+            if (id != friend.FriendId)
+            {
+                return NotFound();
+            }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(friend);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!FriendExists(friend.FriendId))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(friend);
-        //}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _unitOfWork.FriendsRepository.Update(friend);
+                    _unitOfWork.Complete();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (_unitOfWork.FriendsRepository.Get(friend.FriendId) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(friend);
+        }
 
-        //// GET: Friends/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Friends/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //    var friend = await _context.Friend
-        //        .SingleOrDefaultAsync(m => m.FriendId == id);
-        //    if (friend == null)
-        //    {
-        //        return NotFound();
-        //    }
+            var friend = _unitOfWork.FriendsRepository.Get(id.Value);
+            if (friend == null)
+            {
+                return NotFound();
+            }
 
-        //    return View(friend);
-        //}
+            return View(friend);
+        }
 
-        //// POST: Friends/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var friend = await _context.Friend.SingleOrDefaultAsync(m => m.FriendId == id);
-        //    _context.Friend.Remove(friend);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
+        // POST: Friends/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var friend = _unitOfWork.FriendsRepository.Get(id);
+            _unitOfWork.FriendsRepository.Remove(friend);
+            _unitOfWork.Complete();
+            return RedirectToAction(nameof(Index));
+        }
 
-        //private bool FriendExists(int id)
-        //{
-        //    return _context.Friend.Any(e => e.FriendId == id);
-        //}
     }
 }
