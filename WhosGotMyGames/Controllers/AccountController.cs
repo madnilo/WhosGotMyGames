537651a5d7f12cb6a83249ed947f3e26fb5cd41d
Danilo Moura <danilomourag@gmail.com>
2018-02-27 02:50:00 -0300

    -Secure pages -Remove default Home Controller

--- WhosGotMyGames/Controllers/AccountController.cs
+++ WhosGotMyGames/Controllers/AccountController.cs
@@ -247,7 +247,7 @@ namespace WhosGotMyGames.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction(nameof(GamesController.Index), "Games");
         }
 
         [HttpPost]
@@ -335,7 +335,7 @@ namespace WhosGotMyGames.Controllers
         {
             if (userId == null || code == null)
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(GamesController.Index), "Games");
             }
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
@@ -455,7 +455,7 @@ namespace WhosGotMyGames.Controllers
             }
             else
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(GamesController.Index), "Games");
             }
         }
 
